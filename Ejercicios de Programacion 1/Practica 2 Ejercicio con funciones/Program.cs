3a70c4d9eac933d17abb65dfe1ae895ba773e303
Leo-Depablo <leodavincci2004@gmail.com>
2024-01-24 15:55:00 -0800

    Practica 2

---
+++ Ejercicios de Programacion 1/Practica 2 Ejercicio con funciones/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe un año a evaluar");
+string i = Console.ReadLine();
+int a = int.Parse(i.ToString());
+if (a % 4 == 0 && ((a % 100 != 0) | (a % 400 == 0)))
+{
+    Console.WriteLine("Es bisiesto");
+}
+else{
+    Console.WriteLine("no es bisiesto");
+}
