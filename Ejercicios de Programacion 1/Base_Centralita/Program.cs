143c1bc87d12a38ce47868bb44d71422aa574614
Leonardo Depablo Hernández <91438204+Leo-depablo@users.noreply.github.com>
2024-04-07 19:41:50 -0400

    Add files via upload

---
+++ Ejercicios de Programacion 1/Base_Centralita/Program.cs
@@ -0,0 +1,9 @@
+﻿USE Base_Centralita;
+
+CREATE TABLE Llamadas (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    NumeroOrigen VARCHAR(20) NOT NULL,
+    NumeroDestino VARCHAR(20) NOT NULL,
+    Duracion INT NOT NULL,
+    Costo DECIMAL(10,2) NOT NULL
+);
